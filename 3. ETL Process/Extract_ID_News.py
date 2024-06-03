72b5b7512a1fc517426810ee993f4fe6130a7bf9
Rifqi Kurniawan <53246259+RifqiKurniawan@users.noreply.github.com>
2024-06-04 05:24:52 +0700

    Add files via upload

---
+++ 3. ETL Process/Extract_ID_News.py
@@ -0,0 +1,51 @@
+import psycopg2
+import cx_Oracle as cx
+import pandas as pd
+from sqlalchemy import types, create_engine
+import warnings
+warnings.filterwarnings('ignore')
+
+#Connection Configuration
+#Posgre
+conn_pg = psycopg2.connect(
+      database="dataengineer", user='postgres', password='Postgres123', host='10.207.228.107', port= '5432'
+)
+
+cursor_pg = conn_pg.cursor()
+
+#EXTRACT DATA
+df_tr_news = pd.read_sql(f'''
+    SELECT id
+    FROM TR_NEWS
+    ''', con=conn_pg
+)
+
+#TRANSFORM DATA
+
+dtype = {} 
+for j in df_tr_news.columns: 
+    if (df_tr_news[j].dtype == 'object'): 
+        if str(df_tr_news[j].str.len().max()) == 'nan': 
+            dtype[j] = types.VARCHAR(30) 
+        else : 
+            dtype[j] = types.VARCHAR(df_tr_news[j].str.len().max()) 
+    elif df_tr_news[j].dtype == 'float64': 
+            dtype[j] = types.NUMERIC() 
+    elif df_tr_news[j].dtype == 'datetime64[ns]': 
+            dtype[j] = types.DATE()
+
+#LOAD DATA
+connection_string = 'Shrpt/Shrpt23@10.207.231.214:1521/SHRRPT'
+connection_string1= 'oracle+cx_oracle://Shrpt:Shrpt23@10.207.231.214:1521/?service_name=SHRRPT'
+engine = create_engine(connection_string1)
+
+con = cx.connect(connection_string)
+cur = con.cursor()
+
+cur.execute("TRUNCATE TABLE ID_STAGING_TR_NEWS")
+df_tr_news.to_sql('ID_STAGING_TR_NEWS', con=engine, if_exists='append', index=False, dtype=dtype)
+
+con.commit()
+
+con.commit()
+con.close()
